1b32c6bccb981cb1e5672214220a90b072b18051
arvidsson <arvidsson@gmail.com>
2023-06-01 19:35:38 +0200

    tabs to spaces

--- main.go
+++ main.go
@@ -19,10 +19,10 @@ func initialModel() model {
     text := textarea.New()
     text.Focus()
 
-	return model{
+    return model{
         textarea: text,
         err: nil,
-	}
+    }
 }
 
 func (m *model) initModel(width int, height int) {
@@ -36,35 +36,35 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     var cmds []tea.Cmd
-	var cmd tea.Cmd
+    var cmd tea.Cmd
 
-	switch msg := msg.(type) {
+    switch msg := msg.(type) {
     case tea.WindowSizeMsg:
         m.initModel(msg.Width, msg.Height)
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEsc:
-			if m.textarea.Focused() {
-				m.textarea.Blur()
-			}
-		case tea.KeyCtrlC:
-			return m, tea.Quit
-		default:
-			if !m.textarea.Focused() {
-				cmd = m.textarea.Focus()
-				cmds = append(cmds, cmd)
-			}
-		}
+    case tea.KeyMsg:
+        switch msg.Type {
+        case tea.KeyEsc:
+            if m.textarea.Focused() {
+                m.textarea.Blur()
+            }
+        case tea.KeyCtrlC:
+            return m, tea.Quit
+        default:
+            if !m.textarea.Focused() {
+                cmd = m.textarea.Focus()
+                cmds = append(cmds, cmd)
+            }
+        }
 
-	// We handle errors just like any other message
-	case errMsg:
-		m.err = msg
-		return m, nil
-	}
+    // We handle errors just like any other message
+    case errMsg:
+        m.err = msg
+        return m, nil
+    }
 
-	m.textarea, cmd = m.textarea.Update(msg)
-	cmds = append(cmds, cmd)
-	return m, tea.Batch(cmds...)
+    m.textarea, cmd = m.textarea.Update(msg)
+    cmds = append(cmds, cmd)
+    return m, tea.Batch(cmds...)
 }
 
 func (m model) View() string {
